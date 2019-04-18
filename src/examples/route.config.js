58944932af5bd0cf1b2ca1b2926ba49d6f316fc4
mahuimable <mahuimable@163.com>
2019-04-18 18:17:33 +0800

    添加文档路由name

--- src/examples/route.config.js
+++ src/examples/route.config.js
@@ -1,10 +1,16 @@
 import App from './App.vue'
 
 const routeTable = ((r) => {
-	return r.keys().map(key => ({
-		path: key.match(/\.(\/.*)\.md/)[1],
-		component: r(key).default
-	}))
+	// console.log(r.keys())
+	return r.keys().map(key => {
+		// console.log(key)
+		// console.log(r(key))
+		return {
+				path: key.match(/\.(\/.+)\.md/)[1],
+				name: key.match(/\.\/(.+)\.md/)[1],
+				component: r(key).default
+			}
+	})
 })(require.context('./docs', true, /\.md$/))
 
 const routes = [
